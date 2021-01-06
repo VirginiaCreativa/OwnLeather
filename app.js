2930e067576c03b8f04403fe4c404428e88fe141
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-01-05 20:37:47 -0500

    Reactjs Cors Funciona

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const cors = require('cors');
 
 const app = express();
 
@@ -17,10 +17,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors({ origin: true, credentials: true }));
+app.options('*', cors()); 
+app.use(express.static(path.join(__dirname, 'client')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+// ==== STATIC FILES ==== //
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
