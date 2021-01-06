11376229d64bea1767df5fe965fce69d31665f4e
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-01-06 15:38:54 -0500

    Process Login part 1

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Login = () => {
+  console.log(localStorage.getItem('formDatas'));
+  const { namefull, email, password } = JSON.parse(
+    localStorage.getItem('formDatas'),
+  );
   return (
     <>
       <h2>Login</h2>
+      <h1>{namefull}</h1>
     </>
   );
 };
