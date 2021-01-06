8b9f56886f7758b42aabaa27f12057d55d6ff0d8
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-01-06 10:51:06 -0500

    Process Home part 1

--- client/src/containers/home.jsx
+++ client/src/containers/home.jsx
@@ -1,5 +1,19 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Gridolumn = styled.div`
+  display: grid;
+  grid-template-columns: 800px 1fr;
+  height: 100vh;
+`;
+
+const Column = styled.div`
+  overflow: hidden;
+  img {
+    max-width: 105%;
+  }
+`;
 
 const Home = () => {
   const history = useHistory();
@@ -13,7 +27,15 @@ const Home = () => {
   });
   return (
     <>
-      <h1>HOME</h1>
+      <Gridolumn>
+        <Column>
+          <img
+            src="https://images.pexels.com/photos/614971/pexels-photo-614971.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            alt="background leather"
+          />
+        </Column>
+        <Column>Colunm 2</Column>
+      </Gridolumn>
     </>
   );
 };
