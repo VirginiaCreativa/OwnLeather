dbd780e1ffb8620db9f4d4d1884e928685121a34
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-01-07 11:56:18 -0500

    Process User part 5

--- client/src/components/User/UserHeading.jsx
+++ client/src/components/User/UserHeading.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import Variables from '../../styles/VariableStyled';
 
+const Headign = styled.div`
+  margin-bottom: 20px;
+  h1 {
+    color: ${Variables.blueNight2};
+  }
+  h6 {
+    letter-spacing: 1px;
+    color: ${Variables.grey2};
+    font-weight: 300;
+  }
+`;
 const UserHeading = () => {
   const userDatas = useSelector((state) => state.Userign.userDatas);
   return (
-    <>
+    <Headign>
       <h1>{userDatas.namefull}</h1>
-    </>
+      <h6>{userDatas.email}</h6>
+    </Headign>
   );
 };
 
